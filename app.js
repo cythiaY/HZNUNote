da025cad1d99bd649e96411dea95c805415c4401
cythiaY <cythia.xy@gmail.com>
2018-05-29 11:54:24 +0800

    add user authorize

--- app.js
+++ app.js
@@ -6,33 +6,53 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+        success: function (res) {
+          if (res.code) {
+            //发起网络请求
+            wx.request({
+              url: 'https://api.weixin.qq.com/sns/jscode2session',
+              data: {
+                appid: 'wxdc028db00a2ea45d',
+                secret: 'ace2132a76f6157cd31b97bac0e71a16',
+                js_code: res.code,
+                grant_type: 'authorization_code'
+              },
+              success(res) {
+                try {
+                  wx.setStorageSync('openId', res.data.openid)
+                } catch (e) {}
               }
-            }
-          })
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
         }
-      }
-    })
+      }),
+      // 获取用户信息
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
+
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
+              },
+              fail: function (res) {
+                console.log(res)
+              }
+            })
+          }
+        }
+      })
   },
   ajax: function (url, requestData, method) {
     return new Promise((resolve, reject) => {
--- pages/addNote/addNote.js
+++ pages/addNote/addNote.js
@@ -3,18 +3,59 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
+    authVisible: true,
     anonymous: false,
     colorStyle: 'pink'
   },
-  onLoad: function() {},
+  onLoad: function () {
+    var that = this
+    wx.getStorage({
+      key: 'openId',
+      success: function (res) {
+        if (res.data) {} else {}
+      },
+      fail: function (res) {
+        that.setData({
+          authVisible: true
+        })
+      }
+    })
+  },
   toggleAonymous() {
     this.setData({
       anonymous: !this.data.anonymous
     })
   },
-  changeColor(e){
+  changeColor(e) {
     this.setData({
       colorStyle: e.currentTarget.dataset.color
+    })
+  },
+  getUserInfo() {
+    let that = this
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          that.setData({
+            authVisible: false
+          })
+          wx.getUserInfo({
+            success: res => {
+              console.log(res)
+            },
+            fail: res => {
+              console.log(res)
+            }
+          })
+        } else {
+          that.setData({
+            authVisible: true
+          })
+        }
+      },
+      fail: res => {
+        console.log(res)
+      }
     })
   }
 })
